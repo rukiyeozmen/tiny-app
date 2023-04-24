bdafe8b16b4a83ac19887ee2d10eb9de2e19a630
rukiyeozmen <rukiiye42@gmail.com>
2023-04-24 16:54:36 -0700

    added post route to receive form submission

--- express_server.js
+++ express_server.js
@@ -5,18 +5,20 @@ const port = 8080;
 //set view engine for ejs, tells the Express app to use EJS as its templating engine
 app.set('view engine', 'ejs');
 
+// middleware which translates, parses the body of POST in <form>, makes it readable into string. Adds data to req.body 
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//create root route '/' returns hello on the / route
+//create sroot route '/' returns hello on the / route
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-// create '/urls' route passing url data to 'urls_index 'template, When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the below case the key is urls) to access the data within our template.
+// creates '/urls' route passing url data to 'urls_index 'template, When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the below case the key is urls) to access the data within our template.
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
@@ -24,6 +26,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// creates '/urls' route to receive form submission and adds the name='longURL' into req.body as object
+app.post('/urls', (req, res) => {
+  console.log("req.body: ", req.body);
+  res.send('Ok');
+});
+
+//creates 'urls/new' route where urls_new page will be display
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+
+// creates '/urls/:id' routes where :id is the shortURL
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -33,16 +48,20 @@ app.get('/urls/:id', (req, res) => {
 });
 
 
-// creare route '/urls.json' to send the db to the '/urls.json' route
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
 
-//create route '/hello' returns html on /hello route
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
+//! I just put it outside to prevent confusion
+// create route '/urls.json' to send the db to the '/urls.json' route
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+//create route '/hello' returns html on /hello route
+// app.get('/hello', (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
