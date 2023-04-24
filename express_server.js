1d1f938cf713e7452a5e87de2d89c4bd242906c8
rukiyeozmen <rukiiye42@gmail.com>
2023-04-24 14:18:30 -0700

    created 'urls/:id route and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -24,6 +24,13 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render('urls_show', templateVars);
+});
 
 
 // creare route '/urls.json' to send the db to the '/urls.json' route
